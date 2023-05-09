2ee48313acf891c15f340efea2f10a548ed4d455
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-05-09 03:05:54 -0300

    Aula 27 - Controles nos componentes

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -9,6 +9,13 @@ export default {
   args: {
     children: <Text>Testando Elemento</Text>,
   },
+  argTypes: {
+    children: {
+      control: {
+        type: 'null',
+      },
+    },
+  },
 } as Meta<BoxProps>
 
 export const Primary: StoryObj<BoxProps> = {}
